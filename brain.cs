e87afb03bef57da6ad5e9caac62eca72ac8f920e
synx_dv <gomezanthony1607@gmail.com>
2026-06-19 11:25:30 -0400

    Primera version

---
+++ brain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class Brain
+{
+
+    static Dictionary<string, string> respuestas = new Dictionary<string, string>() {
+        {"hola", "ChatBot: Holaaa. En que puedo ayudarte?" },
+        {"como estas?", "ChatBot: Estoy listo para ayudarte!!" }
+    }; 
+    
+
+    public static string RespuestasChatBot(string conenido)
+    {
+        string mensajeUsuario = conenido.ToLower().Trim();
+
+        if (respuestas.ContainsKey(mensajeUsuario))
+        {
+            return respuestas[mensajeUsuario];
+         
+        }
+        else
+        {
+            return "ChatBot: No entiendo lo que quieres decir";
+           
+        }
+    }
+
+}
---
+++ main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Inicio
+{
+
+    public static string nombre;
+    public static bool chateando = true;
+    public static void Main()
+    {
+        Console.WriteLine("ChatBot: Bienvenid@ soy un chatBot creado por Synx.....");
+        while (chateando)
+        {
+            
+            Console.WriteLine("> ");
+            string entrada = Console.ReadLine();
+
+            //Enviar y recibir la respuesta del bot
+            string RespuestaBot = Brain.RespuestasChatBot(entrada);
+            Console.WriteLine(RespuestaBot);
+
+
+  
+        }
+
+        
+    }
+
+
+    
+
+}
